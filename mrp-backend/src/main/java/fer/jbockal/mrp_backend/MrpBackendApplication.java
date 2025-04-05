42c861a79113b2a8da34f9e903ad07a73c6a2e3c
Jan <jbockal1501@gmail.com>
2025-04-05 20:32:18 +0200

    master init

---
+++ mrp-backend/src/main/java/fer/jbockal/mrp_backend/MrpBackendApplication.java
@@ -0,0 +1,13 @@
+package fer.jbockal.mrp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrpBackendApplication.class, args);
+	}
+
+}
---
+++ mrp-backend/src/test/java/fer/jbockal/mrp_backend/MrpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fer.jbockal.mrp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
