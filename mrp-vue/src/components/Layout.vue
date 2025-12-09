5e7470f3f12f8496bca31d8868c7692c3fb1e15d
jbockal <jbockal1501@gmail.com>
2025-12-09 19:17:35 +0100

    vue init

---
+++ mrp-vue/src/components/Layout.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from "vue";
+import TabMenu from "primevue/tabmenu";
+
+const isMobile = ref(false);
+const activeIndex = ref(0);
+const model = ref([
+  "/",
+  "/all-reviews",
+  "/songs",
+  "/albums",
+  "/artists",
+  "/playlists",
+  `/profile`,
+]);
+
+const changeTab = (event) => {
+  activeIndex.value = event.index;
+};
+</script>
+
+<template>
+  <div class="flex flex-column">
+    <TabMenu
+      :pt="{
+        menu: { style: { justifyContent: isMobile ? 'space-between' : '' } },
+        icon: { style: { fontSize: isMobile ? '1.5rem' : '' } },
+      }"
+      :model="model"
+      class="mx-auto text-center w-full md:w-auto"
+      :active-index="activeIndex"
+      @tab-change="changeTab"
+    />
+    <div class="m-5">
+      <router-view />
+    </div>
+  </div>
+</template>
---
+++ mrp-vue/src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import "primeflex/primeflex.css";
+import "./style.css";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
