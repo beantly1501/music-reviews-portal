eaeb0ecf6a642b22f270a9b9374dddc7de23621a
jbockal <jbockal1501@gmail.com>
2026-05-04 17:05:53 +0200

    vue spacing fixes

--- mrp-vue/src/features/newest/Newest.page.vue
+++ mrp-vue/src/features/newest/Newest.page.vue
@@ -10,10 +10,7 @@
       <p>Error loading reviews: {{ error.message }}</p>
     </div>
 
-    <div
-      v-else-if="reviews && reviews.length > 0"
-      class="flex flex-wrap justify-center"
-    >
+    <div v-else-if="reviews && reviews.length > 0" :class="isMobile ? 'flex flex-wrap gap-2 justify-center' : 'flex flex-wrap gap-4 justify-center'">
       <ReviewCard
         v-for="review in reviews"
         :key="review.id"
@@ -31,7 +28,9 @@
 <script setup lang="ts">
 import { ReviewCard } from "@/shared";
 import { ProgressSpinner } from "primevue";
+import { useMediaQuery } from "@vueuse/core";
 import { useGetNewestReviews } from "./hooks/useGetNewestReviews";
 
 const { data: reviews, isLoading, error, refetch } = useGetNewestReviews();
+const isMobile = useMediaQuery("(max-width: 850px)");
 </script>
